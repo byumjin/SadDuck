06bf0e393c1eb58a5458c6ec071ddbc2039dac19
Byumjin Kim <byumjin@seas.upenn.edu>
2018-02-04 03:38:00 -0500

    add duck
    add duck

---
+++ src/main.ts
@@ -0,0 +1,194 @@
+import {vec3, vec4, mat4} from 'gl-matrix';
+import * as Stats from 'stats-js';
+import * as DAT from 'dat-gui';
+import Square from './geometry/Square';
+import Triangular from './geometry/Triangular';
+import Camera from './Camera';
+import {setGL} from './globals';
+import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
+
+// Define an object with application parameters and button callbacks
+// This will be referred to by dat.GUI's functions that add GUI elements.
+const controls = {
+  // TODO: add any controls you want
+  RayMarchStep : 128,
+  ShadowStep : 64,
+  SoftShadow : 8.0,
+
+  ToonShading : 1,
+  EdgeWidth : 0.3,
+
+  AmbientOcculsion : 1,
+  StepLen : 0.12, 
+  Constant: 4.0,
+  
+  Reflection : 0,
+  MaxStep : 40,
+  Intensity : 0.6,
+};
+
+let screenQuad: Square;
+let triangularScreen: Triangular;
+
+let oldTime : number;
+let currentTime : number;
+let elapsedTime : number;
+let deltaTime : number;
+
+function play_single_sound() {
+  var JukeBox = new AudioContext();
+  fetch('music/02_Green_Greens.mp3')
+    .then(r=>r.arrayBuffer())
+    .then(b=>JukeBox.decodeAudioData(b))
+    .then(data=>{
+        const audio_buf = JukeBox.createBufferSource();
+        audio_buf.buffer = data;
+        audio_buf.loop = true;
+        audio_buf.connect(JukeBox.destination);
+        audio_buf.start(0);
+        });
+
+        console.log(`Music On!`);
+}
+
+function main() {
+
+  //play_single_sound();
+
+  elapsedTime = 0.0;
+  oldTime = Date.now();
+
+  // Initial display for framerate
+  const stats = Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+
+  // TODO: add any controls you need to the gui
+  const gui = new DAT.GUI();
+
+  // E.G. gui.add(controls, 'tesselations', 0, 8).step(1);
+  gui.add(controls, 'RayMarchStep', 32.0, 256.0).step(1.0);
+  gui.add(controls, 'ShadowStep', 32.0, 256.0).step(1.0);
+  gui.add(controls, 'SoftShadow', 2.0, 32.0).step(1.0);
+
+  var Toon = gui.addFolder('Toon Shading'); 
+
+  gui.add(controls, 'ToonShading', { Off: 0, On: 1 });
+  gui.add(controls, 'EdgeWidth', 0.0, 1.0).step(0.01);
+  
+
+  var AO = gui.addFolder('AO'); 
+  AO.add(controls, 'AmbientOcculsion', { Off: 0, On: 1 });
+  AO.add(controls, 'StepLen', 0.0, 0.2).step(0.01);
+  AO.add(controls, 'Constant', 0.0, 8.0).step(0.1);
+
+  var reflc = gui.addFolder('Reflection');
+  reflc.add(controls, 'Reflection', { Off: 0, On: 1 });
+  reflc.add(controls, 'MaxStep', 8.0, 64.0).step(1.0);
+  reflc.add(controls, 'Intensity', 0.0, 10.0).step(0.01);
+
+  
+
+  // get canvas and webgl context
+  const canvas = <HTMLCanvasElement> document.getElementById('canvas');
+
+  function setSize(width: number, height: number) {
+    canvas.width = width;
+    canvas.height = height;
+  }
+
+  const gl = <WebGL2RenderingContext> canvas.getContext('webgl2');
+  if (!gl) {
+    alert('WebGL 2 not supported!');
+  }
+  // `setGL` is a function imported above which sets the value of `gl` in the `globals.ts` module.
+  // Later, we can import `gl` from `globals.ts` to access it
+  setGL(gl);
+
+  triangularScreen = new Triangular(vec3.fromValues(0, 0, 0));
+  triangularScreen.create();
+  triangularScreen.bindEnvMap00("src/textures/canivalBG.png");
+
+  const camera = new Camera(vec3.fromValues(-3.7, 2.83528, 12.453), vec3.fromValues(0, 0, 0));
+
+  gl.clearColor(0.0, 0.0, 0.0, 1);
+  gl.disable(gl.DEPTH_TEST);
+
+  const raymarchShader = new ShaderProgram([
+    new Shader(gl.VERTEX_SHADER, require('./shaders/screenspace-vert.glsl')),
+    new Shader(gl.FRAGMENT_SHADER, require('./shaders/raymarch-frag.glsl')),
+  ]);
+
+ 
+
+  function updateTime()
+  {
+    currentTime = Date.now();
+
+    deltaTime = currentTime - oldTime;
+    elapsedTime += deltaTime;    
+
+    oldTime = currentTime;
+  }
+
+  // This function will be called every frame
+  function tick() {
+
+    updateTime();
+
+    camera.update();
+    stats.begin();
+
+    gl.viewport(0, 0, window.innerWidth, window.innerHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // TODO: get / calculate relevant uniforms to send to shader here
+    // TODO: send uniforms to shader
+
+    let viewProj = mat4.create();
+    mat4.multiply(viewProj, camera.projectionMatrix, camera.viewMatrix);
+
+    let invViewProj = mat4.create();
+    mat4.invert(invViewProj, viewProj);
+
+    raymarchShader.setViewMatrix(camera.viewMatrix);
+    raymarchShader.setViewProjMatrix(viewProj);
+    raymarchShader.setinvViewProjMatrix(invViewProj);
+    raymarchShader.setCameraPos(vec4.fromValues( camera.position[0], camera.position[1], camera.position[2], controls.StepLen));
+    raymarchShader.setTimeScreen(vec4.fromValues( elapsedTime, elapsedTime, window.innerWidth, window.innerHeight));
+    raymarchShader.setFactors(vec4.fromValues( controls.ToonShading, controls.SoftShadow, controls.RayMarchStep, controls.ShadowStep));
+
+    raymarchShader.setFactorsAO(vec4.fromValues( controls.AmbientOcculsion, controls.StepLen, controls.Constant, controls.EdgeWidth));
+    raymarchShader.setFactorsReflec(vec4.fromValues( controls.Reflection, controls.MaxStep, controls.Intensity, controls.ShadowStep));
+
+    raymarchShader.setEnvMap00(triangularScreen.envMap00);
+
+    // March!
+    raymarchShader.draw(triangularScreen);
+
+    // TODO: more shaders to layer / process the first one? (either via framebuffers or blending)
+
+    stats.end();
+
+    // Tell the browser to call `tick` again whenever it renders a new frame
+    requestAnimationFrame(tick);
+  }
+
+  window.addEventListener('resize', function() {
+    setSize(window.innerWidth, window.innerHeight);
+    camera.setAspectRatio(window.innerWidth / window.innerHeight);
+    camera.updateProjectionMatrix();
+  }, false);
+
+  setSize(window.innerWidth, window.innerHeight);
+  camera.setAspectRatio(window.innerWidth / window.innerHeight);
+  camera.updateProjectionMatrix();
+
+  // Start the render loop
+  tick();
+}
+
+main();
---
+++ src/rendering/gl/ShaderProgram.ts
@@ -0,0 +1,169 @@
+import {vec4, mat4} from 'gl-matrix';
+import Drawable from './Drawable';
+import {gl} from '../../globals';
+
+var activeProgram: WebGLProgram = null;
+
+export class Shader {
+  shader: WebGLShader;
+
+  constructor(type: number, source: string) {
+    this.shader = gl.createShader(type);
+    gl.shaderSource(this.shader, source);
+    gl.compileShader(this.shader);
+
+    if (!gl.getShaderParameter(this.shader, gl.COMPILE_STATUS)) {
+      throw gl.getShaderInfoLog(this.shader);
+    }
+  }
+};
+
+class ShaderProgram {
+  prog: WebGLProgram;
+
+  attrPos: number;
+  attrUV: number;
+
+  unifView: WebGLUniformLocation;
+  unifViewProj: WebGLUniformLocation;
+  unifinvViewProj : WebGLUniformLocation;
+  unifCameraPos : WebGLUniformLocation;
+  unifTimeScreen : WebGLUniformLocation;
+
+  unifFactors : WebGLUniformLocation;
+
+  unifFactorsAO : WebGLUniformLocation;
+  unifFactorsReflec : WebGLUniformLocation;
+  
+  unifEnvMap00: WebGLUniformLocation;
+
+  constructor(shaders: Array<Shader>) {
+    this.prog = gl.createProgram();
+
+    for (let shader of shaders) {
+      gl.attachShader(this.prog, shader.shader);
+    }
+    gl.linkProgram(this.prog);
+    if (!gl.getProgramParameter(this.prog, gl.LINK_STATUS)) {
+      throw gl.getProgramInfoLog(this.prog);
+    }
+
+    // Raymarcher only draws a quad in screen space! No other attributes
+    this.attrPos = gl.getAttribLocation(this.prog, "vs_Pos");
+    this.attrUV = gl.getAttribLocation(this.prog, "vs_UV");
+
+    // TODO: add other attributes here
+    this.unifView   = gl.getUniformLocation(this.prog, "u_View");
+    this.unifViewProj   = gl.getUniformLocation(this.prog, "u_ViewProj");
+    this.unifinvViewProj   = gl.getUniformLocation(this.prog, "u_InvViewProj");
+    
+
+    this.unifCameraPos   = gl.getUniformLocation(this.prog, "u_CameraPos");
+    this.unifTimeScreen   = gl.getUniformLocation(this.prog, "u_TimeScreen");
+    this.unifFactors   = gl.getUniformLocation(this.prog, "u_Factors");
+
+    this.unifFactorsAO = gl.getUniformLocation(this.prog, "u_FactorsAO");
+    this.unifFactorsReflec = gl.getUniformLocation(this.prog, "u_FactorsReflec");
+
+    this.unifEnvMap00   = gl.getUniformLocation(this.prog, "u_EnvMap");
+
+  }
+
+  use() {
+    if (activeProgram !== this.prog) {
+      gl.useProgram(this.prog);
+      activeProgram = this.prog;
+    }
+  }
+
+  // TODO: add functions to modify uniforms
+  setViewMatrix(vp: mat4) {
+    this.use();
+    if (this.unifView !== -1) {
+      gl.uniformMatrix4fv(this.unifView, false, vp);
+    }
+  }
+ 
+  setViewProjMatrix(vp: mat4) {
+    this.use();
+    if (this.unifViewProj !== -1) {
+      gl.uniformMatrix4fv(this.unifViewProj, false, vp);
+    }
+  }
+
+  setinvViewProjMatrix(vp: mat4) {
+    this.use();
+    if (this.unifinvViewProj !== -1) {
+      gl.uniformMatrix4fv(this.unifinvViewProj, false, vp);
+    }
+  }
+
+  setCameraPos(vec: vec4) {
+    this.use();
+    if (this.unifCameraPos !== -1) {
+      gl.uniform4fv(this.unifCameraPos, vec);
+    }
+  }
+
+  setTimeScreen(vec: vec4) {
+    this.use();
+    if (this.unifTimeScreen !== -1) {
+      gl.uniform4fv(this.unifTimeScreen, vec);
+    }
+  }
+
+  setFactors(vec: vec4) {
+    this.use();
+    if (this.unifFactors !== -1) {
+      gl.uniform4fv(this.unifFactors, vec);
+    }
+  }
+
+  setFactorsAO(vec: vec4) {
+    this.use();
+    if (this.unifFactorsAO !== -1) {
+      gl.uniform4fv(this.unifFactorsAO, vec);
+    }
+  }
+
+  setFactorsReflec(vec: vec4) {
+    this.use();
+    if (this.unifFactorsReflec !== -1) {
+      gl.uniform4fv(this.unifFactorsReflec, vec);
+    }
+  }
+  setEnvMap00(texture: WebGLTexture) {
+    this.use();
+    if (this.unifEnvMap00 !== -1) {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.uniform1i(gl.getUniformLocation(this.prog, "u_EnvMap"), 0);
+    }
+}
+
+
+
+  draw(d: Drawable) {
+    this.use();
+
+    if (this.attrPos != -1 && d.bindPos()) {
+      gl.enableVertexAttribArray(this.attrPos);
+      gl.vertexAttribPointer(this.attrPos, 4, gl.FLOAT, false, 0, 0);
+    }
+
+    if (this.attrUV != -1 && d.bindUV()) {
+      gl.enableVertexAttribArray(this.attrUV);
+      gl.vertexAttribPointer(this.attrUV, 2, gl.FLOAT, false, 0, 0);
+    }
+
+
+
+    d.bindIdx();
+    gl.drawElements(d.drawMode(), d.elemCount(), gl.UNSIGNED_INT, 0);
+
+    if (this.attrPos != -1) gl.disableVertexAttribArray(this.attrPos);
+    if (this.attrUV != -1) gl.disableVertexAttribArray(this.attrUV);
+
+  }
+};
+
+export default ShaderProgram;
